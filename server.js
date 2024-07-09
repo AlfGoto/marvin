33161f3483df9c8e9d8ce6639a5950f9749d3a2d
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-07-09 23:32:31 +0200

    express

---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+    sendContact()
+});
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
+
+async function sendContact() {
+
+    const webhookBody = {
+        title: 'ok',
+        embeds: [{
+            fields: [
+                { name: getCurrentTimeInFrance(), value: req.get('host') }
+            ]
+        }],
+    };
+
+    const webhookUrl = 'https://discord.com/api/webhooks/1260315791237316620/wRe2uqiHwAAi3K5mfPiNPjFTxCzv4PXz7rtIwF9gLzKOeHNu8o74VYG--xRzs9VAyNcZ';
+
+    const response = await fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(webhookBody),
+    });
+}
+sendContact()
+
+
+
+function getCurrentTimeInFrance() {
+    const now = new Date()
+    const options = {
+        hour: '2-digit',
+        minute: '2-digit',
+        second: '2-digit',
+        timeZone: 'Europe/Paris',
+        hour12: false
+    };
+    const timeFormatter = new Intl.DateTimeFormat('fr-FR', options);
+    const formattedTime = timeFormatter.format(now);
+
+    return formattedTime;
+}
