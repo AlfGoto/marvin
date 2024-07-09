b8eae72747c0734f2ae63038748c79d785121e22
AlfGoto <147511905+AlfGoto@users.noreply.github.com>
2024-07-09 21:37:58 +0200

    ok

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
-import url from './env.js'
+async function sendContact() {
 
-console.log(url)
+    const webhookBody = {
+        title: 'ok',
+        embeds: [{
+            fields: [
+                { name: 'bref', value: ('hello') }
+            ]
+        }],
+    };
+
+    const webhookUrl = 'https://discord.com/api/webhooks/1260315791237316620/wRe2uqiHwAAi3K5mfPiNPjFTxCzv4PXz7rtIwF9gLzKOeHNu8o74VYG--xRzs9VAyNcZ';
+
+    const response = await fetch(webhookUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(webhookBody),
+    });
+    
+    document.getElementById('messageInput').value = ''
+    if (response.ok) {
+        alert('Message envoyé !')
+
+    } else {
+        alert('bon là ya un soucis');
+    }
+}
+sendContact()
